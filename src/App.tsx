c218e644360cf49a11c1324d2799bd4f8c1aa250
khatabakhsh <ah.khatabakhsh@yarhis.com>
2025-07-19 11:24:51 +0330

    fix styles and clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,95 +1,98 @@
 import "./App.scss";
 import { useState } from "react";
 import { defaultData } from "./data";
+import { ISOtoString } from "./utils/dates";
 
 function App() {
   const [data] = useState(() => [...Object.values(defaultData.data.list)]);
 
   return (
     <main>
-      <table>
-        <thead>
-          <tr className="main-headers">
-            <th colSpan={2}>Lorem ipsum</th>
-            <th colSpan={8}>Your Empties</th>
-            <th colSpan={7}>Your Exports</th>
-          </tr>
-          <tr className="sub-headers">
-            <th>Size</th>
-            <th>SSL</th>
-
-            <th>Container</th>
-            <th>Chasis</th>
-            <th>Gated Out</th>
-            <th>Gated In</th>
-            <th>Return Terminal</th>
-            <th>Return Apt</th>
-            <th>Est. PerDiem</th>
-            <th> Empty Location</th>
-
-            <th>Booking</th>
-            <th>Loc</th>
-            <th>Terminal Apt</th>
-            <th>ERD</th>
-            <th>Cut Off</th>
-            <th>Pickup Loc</th>
-            <th>Saving</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item) => (
-            <tr key={item.cabin.id}>
-              <td id="cell_size_ssl" colSpan={2}>
-                <div>
-                  <td>{item.size_type}</td>
-                  <td>{item.company_name}</td>
-                </div>
-              </td>
-
-              <td id="cell_empties" colSpan={8}>
-                {item.empties.map(({ containers }) =>
-                  containers.map((container) => (
-                    <div>
-                      <td>{container.name}</td>
-                      <td>{container.chases}</td>
-                      <td>
-                        {new Date(container.gated_out).toLocaleDateString(
-                          "en-US"
-                        )}
-                      </td>
-                      <td>
-                        {new Date(container?.gated_in || "").toLocaleDateString(
-                          "en-US"
-                        )}
-                      </td>
-                      <td>{container.return_terminals}</td>
-                      <td>{container.return_appointment || "-"}</td>
-                      <td>{container.est_per_diem || "-"}</td>
-                      <td> </td>
-                    </div>
-                  ))
-                )}
-              </td>
-
-              <td id="cell_exports" colSpan={7}>
-                {item.exports.map(({ bookings }) =>
-                  bookings.map((booking) => (
-                    <div>
-                      <td>{booking.name}</td>
-                      <td>{booking.pick_up_location}</td>
-                      <td>{booking.return_to_terminal?.name || "-"}</td>
-                      <td>{booking.erd || "-"}</td>
-                      <td>{booking.cutoff}</td>
-                      <td>{booking.pick_up_location || "-"}</td>
-                      <td> </td>
-                    </div>
-                  ))
-                )}
-              </td>
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr className="main-headers">
+              <th colSpan={2}>Lorem ipsum</th>
+              <th colSpan={7}>Your Empties</th>
+              <th colSpan={5}>Your Exports</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+            <tr className="sub-headers">
+              <th id="size">Size</th>
+              <th id="ssl">SSL</th>
+
+              <th id="container">Container</th>
+              <th id="chasis">Chasis</th>
+              <th id="gated-out">Gated Out</th>
+              <th id="gated-in">Gated In</th>
+              <th id="return-terminal">Return Terminal</th>
+              <th id="return-apt">Return Apt</th>
+              <th id="perdiem">Est. PerDiem</th>
+
+              <th id="booking">Booking</th>
+              <th id="erd">ERD</th>
+              <th id="cutoff">Cut Off</th>
+              <th id="pickup-loc">Pickup Loc</th>
+              <th id="saving">Saving</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr key={item.cabin.id + item.size_type + item.company_name}>
+                <td id="cell_size_ssl" colSpan={2}>
+                  <div className="table-row">
+                    <td headers="size">{item.size_type}</td>
+                    <td headers="ssl">{item.company_name}</td>
+                  </div>
+                </td>
+
+                <td id="cell_empties" colSpan={7}>
+                  {item.empties.map(({ containers }) =>
+                    containers.map((container) => (
+                      <div key={container.id} className="table-row">
+                        <td headers="container">{container.name}</td>
+                        <td headers="chasis">{container.chases}</td>
+                        <td headers="gated-out">
+                          {ISOtoString(container.gated_out) || "-"}
+                        </td>
+                        <td headers="gated-in">
+                          {ISOtoString(container.gated_in) || "-"}
+                        </td>
+                        <td headers="return-terminal">
+                          {container.return_terminals || "-"}
+                        </td>
+                        <td headers="return-apt">
+                          {container.return_appointment || "-"}
+                        </td>
+                        <td headers="perdiem">
+                          {container.est_per_diem || "-"}
+                        </td>
+                      </div>
+                    ))
+                  )}
+                </td>
+
+                <td id="cell_exports" colSpan={5}>
+                  {item.exports.map(({ bookings }) =>
+                    bookings.map((booking) => (
+                      <div key={booking.id} className="table-row">
+                        <td headers="booking">{booking.name}</td>
+                        <td headers="erd">{ISOtoString(booking.erd) || "-"}</td>
+                        <td headers="cutoff">
+                          {ISOtoString(booking.cutoff) || "-"}
+                        </td>
+                        <td headers="pickup-loc">
+                          {booking.pick_up_location || "-"}
+                        </td>
+                        <td headers="saving"> </td>
+                      </div>
+                    ))
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </main>
   );
 }
